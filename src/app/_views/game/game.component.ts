3f9122108d2913cc131dec94f1244f76e1646eba
DavidSandor <sandor.david89@gmail.com>
2021-04-29 17:26:23 +0200

    Game update with card click

--- src/app/_views/game/game.component.ts
+++ src/app/_views/game/game.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GameCard } from 'src/app/_models/game-card.model';
+import { GameService } from 'src/app/_services/game.service';
 
 @Component({
   selector: 'app-game',
@@ -9,9 +11,18 @@ export class GameComponent implements OnInit {
 
   public isGameOn: boolean = true;
 
-  constructor() { }
+  public cardDeck: GameCard[] = [];
+
+  constructor(private gameService: GameService) {
+    this.gameService.gameState$.subscribe(gameState => {
+      this.cardDeck = gameState.cardDeck;
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  public onCardClicked(card: GameCard) {
+    this.gameService.revealCard(card);
+  }
 }
