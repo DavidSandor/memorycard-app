f2c9b8ee7263a90cd3152971cb4f57e04c90ecd8
DavidSandor <sandor.david89@gmail.com>
2021-04-29 17:24:33 +0200

    Store update: flipcard + matchcards actions

--- src/app/_store/game-state/game-state.reducer.ts
+++ src/app/_store/game-state/game-state.reducer.ts
@@ -13,7 +13,23 @@ const _gameStateReducer = createReducer(
     on(GameStateActions.setCardDeck, (state, {deck}) => ({...state, cardDeck: deck})),
     on(GameStateActions.setBestTries, (state, {tries}) => ({ ...state, bestTries: tries })),
     on(GameStateActions.setCurrentTries, (state, {tries}) => ({ ...state, currentTries: tries })),
-    on(GameStateActions.resetGame, (state, {deck}) => ({ ...state, cardDeck: deck, currentTries: 0}))
+    on(GameStateActions.resetGame, (state, {deck}) => ({ ...state, cardDeck: deck, currentTries: 0})),
+    on(GameStateActions.flipCard, (state, {cardId, isRevealed}) => {
+        return {
+            ...state,
+            cardDeck: state.cardDeck.map(cardOrigi => {
+                return cardOrigi.id === cardId ? {...cardOrigi, isRevealed} : cardOrigi;
+            })
+        };
+    }),
+    on(GameStateActions.matchCards, (state, {cardName}) => {
+        return {
+            ...state,
+            cardDeck: state.cardDeck.map((cardOrigi) => {
+                return cardOrigi.cardName === cardName ? {...cardOrigi, isRevealed: true, isMatched: true} : cardOrigi;
+            })
+        };
+    })
 );
   
   export function reducer(state: GameState | undefined, action: Action) {
